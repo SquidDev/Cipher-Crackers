e60d7a011ad152dc0e5ae3f371af643dab451e80
SquidDev <bonzoweb@hotmail.co.uk>
2014-09-21 11:45:31 +0100

    Add RailFence code

---
+++ C#/Cipher/Ciphers/RailFence.cs
@@ -0,0 +1,74 @@
+﻿using Cipher.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cipher.Ciphers
+{
+    public class RailFence<TArray> : BaseCipher<int, TArray, byte>
+        where TArray : TextArray<byte>, new()
+    {
+        public RailFence(string CipherText) : base(CipherText) { }
+        public RailFence(TArray CipherText) : base(CipherText) { }
+
+        public override TArray Decode(int Key, TArray Decoded)
+        {
+            int Length = Text.Length;
+            Key--;
+
+            int K = 0;
+            int Diff = 2 * Key;
+            for(int Line = 0; Line < Key; Line++)
+            {
+                int Skip = 2 * (Key - Line);
+                int J = 0;
+                for (int I = Line; I < Length; )
+                {
+                    Decoded[I] = Text[K++];
+                    if (Line == 0 || J % 2 == 0)
+                    {
+                        I += Skip;
+                    }
+                    else
+                    {
+                        I += Diff - Skip;
+                    }
+                    J++;
+                }
+            }
+            for (int I = Key; I < Length; I+=Diff)
+            {
+                Decoded[I] = Text[K++];
+            }
+
+            return Decoded;
+        }
+
+        public override CipherResult Crack()
+        {
+            double BestScore = Double.NegativeInfinity;
+            int BestKey = 0;
+
+            int Length = Text.Length;
+
+            TArray Decoded = new TArray();
+            Decoded.Initalise(Length);
+
+            Length++;
+            for (int Key = 2; Key < Length; Key++)
+            {
+                Decoded = Decode(Key, Decoded);
+                double Score = Decoded.ScoreText();
+
+                if (Score > BestScore)
+                {
+                    BestScore = Score;
+                    BestKey = Key;
+                }
+            }
+
+            return GetResult(BestScore, BestKey, Decoded);
+        }
+    }
+}
---
+++ C#/Testing/Ciphers/RailFenceTest.cs
@@ -0,0 +1,61 @@
+﻿using Cipher.Ciphers;
+using Cipher.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing.Ciphers
+{
+    [TestClass]
+    public class RailFenceTest : CipherTest<int>
+    {
+        /// <summary>
+        /// Tests the crack method
+        /// </summary>
+        [TestMethod]
+        [DeploymentItem(@"TestData\Cipher-RailFence.xml")]
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.XML",
+            @"|DataDirectory|\TestData\Cipher-RailFence.xml", "Cipher",
+            DataAccessMethod.Sequential
+        )]
+        public void RailFenceCrack()
+        {
+            InternalCrack(DataRead("Ciphertext"), DataRead("Plaintext"), DataReadByte("Key"));
+        }
+
+
+        /// <summary>
+        /// Tests the decode method
+        /// </summary>
+        [TestMethod]
+        [DeploymentItem(@"TestData\Cipher-RailFence.xml")]
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.XML",
+            @"|DataDirectory|\TestData\Cipher-RailFence.xml", "Cipher",
+            DataAccessMethod.Sequential
+        )]
+        public void RailFenceDecode()
+        {
+            InternalDecode(DataRead("Ciphertext"), DataRead("Plaintext"), DataReadByte("Key"));
+        }
+
+        #region Internal functions
+        protected override void InternalCrack(string Ciphertext, string Plaintext, int Key)
+        {
+            RailFence<QuadgramScoredLetterArray> Shift = new RailFence<QuadgramScoredLetterArray>(Ciphertext);
+            RailFence<QuadgramScoredLetterArray>.CipherResult Result = Shift.Crack();
+
+
+            Assert.AreEqual(Plaintext, Result.Text.ToString());
+            Assert.AreEqual<int>(Key, Result.Key);
+        }
+
+        protected override void InternalDecode(string Ciphertext, string Plaintext, int Key)
+        {
+            RailFence<LetterArray> Shift = new RailFence<LetterArray>(Ciphertext);
+            LetterArray Result = Shift.Decode(Key);
+
+            Assert.AreEqual(Plaintext, Result.ToString());
+        }
+        #endregion
+    }
+}
