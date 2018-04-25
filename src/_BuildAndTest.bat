be7f5a9380e5224bfc108714d73bc185298ba079
SquidDev <bonzoweb@hotmail.co.uk>
2018-04-25 08:23:12 +0100

    Update license
    Also remove some random junk which shouldn't be here

--- src/_BuildAndTest.bat
+++ src/_BuildAndTest.bat
@@ -1,3 +1,2 @@
 @set PATH=%PATH%;"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow";"C:\Program Files (x86)\MSBuild\12.0\bin\"
 MSBuild.exe "Cipher.sln" /verbosity:minimal && vstest.console "Testing\bin\Release\Testing.dll"
-pause
